c031264ffde80b1538dcf10c0eaaa202f6444df3
Кирилл Нестерович <nesterkalol@gmail.com>
2022-05-26 17:21:54 +0300

    modified users api methods

--- YourVitebskWebServiceApp/APIControllers/UsersController.cs
+++ YourVitebskWebServiceApp/APIControllers/UsersController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using YourVitebskWebServiceApp.APIServiceInterfaces;
-using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using YourVitebskWebServiceApp.APIModels;
@@ -21,39 +20,11 @@ namespace YourVitebskWebServiceApp.APIControllers
             _usersService = usersService;
         }
 
-        // Gets all users
-        [HttpGet("all")]
-        public async Task<IEnumerable<User>> GetAllUsers()
+        // Gets all users except the caller
+        [HttpGet("all/{id}")]
+        public async Task<IEnumerable<UsersListItem>> GetAllUsers(int id)
         {
-            return await _usersService.GetAllUsers();
-        }
-
-        // Gets user by id
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ResponseModel>> GetUser(int id)
-        {
-            User user = await _usersService.GetById(id);
-            if (user == null)
-            {
-                return NotFound(ResponseModel.CreateResponseWithError("Пользователь не найден!"));
-            }
-
-            return Ok(ResponseModel.CreateResponseWithContent(user));
-        }
-
-        // Updates user
-        [HttpPut]
-        public async Task<IActionResult> UpdateUser(User user)
-        {
-            try
-            {
-                await _usersService.Update(user);
-                return Ok();
-            }
-            catch (ArgumentException e)
-            {
-                return BadRequest(ResponseModel.CreateResponseWithError(e.Message));
-            }
+            return await _usersService.GetAllUsers(id);
         }
     }
 }
